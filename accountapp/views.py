8c74f355da3017a0f51ed3b1905c69cb11181b9a
Grayson-choi <784wldnd@gmail.com>
2021-01-18 14:27:05 +0900

    AccountCreateView 추가

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,32 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        # 그냥 render를 쓰면 새로고침 할때마다 데이터가 저장되게됨 이를 방지하기 위해 Httpresponse Redirect를 사
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
